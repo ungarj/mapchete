ef1137ac79db16e30851bc3c1247f3c42f5d9882
ungarj <joachim.ungar@gmail.com>
2015-07-30 12:29:49 +0200

    adding first tests & fixing bugs in tilematrix module

--- mapchete/tests.py
+++ mapchete/tests.py
@@ -1,23 +1,121 @@
 #!/usr/bin/env python
 
 import sys
-from shapely.geometry import Polygon, shape
+import os
+import fiona
+from shapely.geometry import *
 from shapely.wkt import loads
 from tilematrix import *
+
 
 ROUND = 10
 
 def main(args):
 
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    testdata_directory = os.path.join(scriptdir, "testdata")
+    outdata_directory = os.path.join(testdata_directory, "out")
     wgs84 = TileMatrix("4326")
 
-    print "tiles for zoomlevel: %s, %s" %(wgs84.tiles_for_zoom(5))
-    print "top left coordinate of tile: %s, %s" %(wgs84.top_left_tile_coords(3, 3, 5))
-    print "tile boundaries: %s" %(wgs84.tile_bounds(3, 3, 5))
-    bbox_polygon = loads("POLYGON ((-146.25 81.5625, 213.75 81.5625, 213.75 -98.4375, -146.25 -98.4375, -146.25 81.5625))")
-    print "tiles from bbox: %s" %(tiles_from_bbox(wgs84, bbox_polygon, 2))
-    polygon = loads("POLYGON ((-146.25 81.5625, 213.75 81.5625, 213.75 -98.4375, -146.25 -98.4375, -146.25 81.5625))")
-    print "tiles from geometry: %s" %(tiles_from_geom(wgs84, polygon, 2))
+    # tiles per zoomlevel
+    try:
+        tiles = wgs84.tiles_for_zoom(5)
+        assert tiles == (64, 32)
+        print "tiles per zoomlevel OK"
+    except:
+        print "tiles per zoomlevel FAILED"
+
+    # top left coordinate
+    try:
+        tl = wgs84.top_left_tile_coords(3, 3, 5)
+        print tl
+        assert tl == (-163.125, 73.125)
+        print "top left coordinate OK"
+    except:
+        print "top left coordinate FAILED"
+
+
+    # tile boundaries
+    try:
+        boundaries = wgs84.tile_bounds(3, 3, 5)
+        testpolygon = Polygon([[-163.125, 73.125], [-157.5, 73.125],
+            [-157.5, 67.5], [-163.125, 67.5], [-163.125, 73.125]])
+        assert boundaries.equals(testpolygon)
+        print "tile boundaries OK"
+    except:
+        print "tile boundaries FAILED"
+    
+
+    # test bounding box
+    bbox_location = os.path.join(testdata_directory, "bbox.geojson")
+    tiled_out = os.path.join(outdata_directory, "bbox_tiles.geojson")
+    zoom = 5
+    testtiles = [(33, 5), (33, 6), (33, 7), (33, 8), (33, 9), (33, 10),
+        (34, 5), (34, 6), (34, 7), (34, 8), (34, 9), (34, 10), (35, 5),
+        (35, 6), (35, 7), (35, 8), (35, 9), (35, 10), (36, 5), (36, 6),
+        (36, 7), (36, 8), (36, 9), (36, 10), (37, 5), (37, 6), (37, 7),
+        (37, 8), (37, 9), (37, 10), (38, 5), (38, 6), (38, 7), (38, 8),
+        (38, 9), (38, 10), (39, 5), (39, 6), (39, 7), (39, 8), (39, 9),
+        (39, 10), (40, 5), (40, 6), (40, 7), (40, 8), (40, 9), (40, 10),
+        (41, 5), (41, 6), (41, 7), (41, 8), (41, 9), (41, 10)]
+    with fiona.open(bbox_location) as bbox_file:
+        try:
+            bbox_tiles = wgs84.tiles_from_bbox(bbox_file, zoom)
+            assert bbox_tiles == testtiles
+            print "bounding box OK"
+        except:
+            print "bounding box FAILED"
+    ## write debug output
+    schema = {
+        'geometry': 'Polygon',
+        'properties': {'col': 'int', 'row': 'int'}
+    }
+    try:
+        os.remove(tiled_out)
+    except:
+        pass
+    with fiona.open(tiled_out, 'w', 'GeoJSON', schema) as sink:
+        for tile in bbox_tiles:
+            col, row = tile
+            feature = {}
+            feature['geometry'] = mapping(wgs84.tile_bounds(col, row, zoom))
+            feature['properties'] = {}
+            feature['properties']['col'] = col
+            feature['properties']['row'] = row
+            sink.write(feature)
+
+    # tiles from point
+    point_location = os.path.join(testdata_directory, "point.geojson")
+    tiled_out = os.path.join(outdata_directory, "point_tiles.geojson")
+    zoom = 6
+    testtile = [(69, 14)]
+    with fiona.open(point_location) as point_file:
+        point = shape(point_file[0]["geometry"])
+        try:
+            point_tile = wgs84.tiles_from_geom(point, zoom)
+            assert bbox_tiles == testtiles
+            print "Point OK"
+        except:
+            print "Point FAILED"
+    ## write debug output
+    schema = {
+        'geometry': 'Point',
+        'properties': {'col': 'int', 'row': 'int'}
+    }
+    try:
+        os.remove(tiled_out)
+    except:
+        pass
+    with fiona.open(tiled_out, 'w', 'GeoJSON', schema) as sink:
+        col, row = point_tile[0]
+        feature = {}
+        feature['geometry'] = mapping(wgs84.tile_bounds(col, row, zoom))
+        feature['properties'] = {}
+        feature['properties']['col'] = col
+        feature['properties']['row'] = row
+        sink.write(feature)
+
+    #print "tiles from geometry: %s" %(tiles_from_geom(wgs84, polygon, 2))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- mapchete/tilematrix.py
+++ mapchete/tilematrix.py
@@ -38,14 +38,14 @@ class TileMatrix:
             return wetiles, nstiles
 
     def tilesize_for_zoom(self, zoom):
-        nstiles, wetiles = self.tiles_for_zoom(zoom)
+        wetiles, nstiles = self.tiles_for_zoom(zoom)
         try:
             assert isinstance(zoom, int)
         except:
             print "Zoom (%s) must be an integer." %(zoom)
             sys.exit(0)
-        tile_nssize = float(round(self.nssize/nstiles, ROUND))
         tile_wesize = float(round(self.wesize/wetiles, ROUND))
+        tile_nssize = float(round(self.nssize/nstiles, ROUND))
         return tile_wesize, tile_nssize
 
     def top_left_tile_coords(self, col, row, zoom):
@@ -68,94 +68,100 @@ class TileMatrix:
         except:
             print "Zoom (%s) must be an integer." %(zoom)
             sys.exit(0)
+        tile_wesize, tile_nssize = self.tilesize_for_zoom(zoom)
         ul = self.top_left_tile_coords(col, row, zoom)
-        ur = ul[0] + self.wesize, ul[1]
-        lr = ul[0] + self.wesize, ul[1] - self.nssize
-        ll = ul[0], ul[1] - self.nssize
+        ur = ul[0] + tile_wesize, ul[1]
+        lr = ul[0] + tile_wesize, ul[1] - tile_nssize
+        ll = ul[0], ul[1] - tile_nssize
     
         return Polygon([ul, ur, lr, ll])
 
 
-def tiles_from_bbox(tilematrix, geometry, zoom):
-    try:
-        assert isinstance(zoom, int)
-    except:
-        print "Zoom (%s) must be an integer." %(zoom)
-        sys.exit(0)
-    tile_wesize, tile_nssize = tilematrix.tilesize_for_zoom(zoom)
-    tilelist = []
-    l, b, r, t = geometry.bounds
-    tilelon = tilematrix.left
-    tilelat = tilematrix.top
-    cols = []
-    rows = []
-    col = -1
-    row = -1
-    while tilelon <= l:
-        tilelon += tile_wesize
-        col += 1
-    cols.append(col)
-    while tilelon < r:
-        tilelon += tile_wesize
-        col += 1
-        cols.append(col)
-    while tilelat >= t:
-        tilelat -= tile_nssize
-        row += 1
-    rows.append(row)
-    while tilelat > b:
-        tilelat -= tile_nssize
-        row += 1
-        rows.append(row)
-    tilelist = list(product(cols, rows))   
-    
-    return tilelist
-
-
-def tiles_from_geom(tilematrix, geometry, zoom):
-    # returns tiles intersecting with input geometry
-
-    tilelist = []
-
-    if geometry.almost_equals(geometry.envelope, ROUND):
-
-        tilelist = tiles_from_bbox(tilematrix, geometry, zoom)
-        print "pompete"
-
-    elif geometry.geom_type == "Point":
-
-        lon, lat = list(geometry.coords)[0]
-
-        tilelon = tilematrix.left
-        tilelat = tilematrix.top
-
+    def tiles_from_bbox(self, geometry, zoom):
+        try:
+            assert isinstance(zoom, int)
+        except:
+            print "Zoom (%s) must be an integer." %(zoom)
+            sys.exit(0)
+        tile_wesize, tile_nssize = self.tilesize_for_zoom(zoom)
+        tilelist = []
+        l, b, r, t = geometry.bounds
+        tilelon = self.left
+        tilelat = self.top
+        cols = []
+        rows = []
         col = -1
         row = -1
-    
-        while tilelon < lon:
-            tilelon += tile.wesize
+        while tilelon <= l:
+            tilelon += tile_wesize
             col += 1
-    
-        while tilelat > lat:
-            tilelat -= tile.nssize
+        cols.append(col)
+        while tilelon < r:
+            tilelon += tile_wesize
+            col += 1
+            cols.append(col)
+        while tilelat >= t:
+            tilelat -= tile_nssize
             row += 1
+        rows.append(row)
+        while tilelat > b:
+            tilelat -= tile_nssize
+            row += 1
+            rows.append(row)
+        tilelist = list(product(cols, rows))   
+        
+        return tilelist
     
-        tilelist.append((col, row))
+    
+    def tiles_from_geom(self, geometry, zoom):
+        # returns tiles intersecting with input geometry
+    
+        tilelist = []
 
-    elif geometry.geom_type in ("LineString", "MultiLineString", "Polygon",
-        "MultiPolygon", "MultiPoint"):
+        try:
+            assert geometry.is_valid
+        except:
+            print "geometry is not valid"
+    
+        if geometry.almost_equals(geometry.envelope, ROUND):
+    
+            tilelist = self.tiles_from_bbox(geometry, zoom)
 
-        prepared_geometry = prep(geometry)
-        bbox_tilelist = tiles_from_bbox(tilematrix, geometry, zoom)  
-        for tile in bbox_tilelist:
-            col, row = tile
-            print tile
-            geometry = tilematrix.tile_bounds(col, row, zoom)
-            if prepared_geometry.intersects(geometry):
-                tilelist.append((col, row))
-
-    else:
-        print "ERROR: no valid geometry"
-        sys.exit(0)
-
-    return tilelist
+    
+        elif geometry.geom_type == "Point":
+    
+            lon, lat = list(geometry.coords)[0]
+    
+            tilelon = self.left
+            tilelat = self.top
+    
+            col = -1
+            row = -1
+        
+            while tilelon < lon:
+                tilelon += tile.wesize
+                col += 1
+        
+            while tilelat > lat:
+                tilelat -= tile.nssize
+                row += 1
+        
+            tilelist.append((col, row))
+    
+        elif geometry.geom_type in ("LineString", "MultiLineString", "Polygon",
+            "MultiPolygon", "MultiPoint"):
+    
+            prepared_geometry = prep(geometry)
+            bbox_tilelist = self.tiles_from_bbox(geometry, zoom)  
+            for tile in bbox_tilelist:
+                col, row = tile
+                print tile
+                geometry = self.tile_bounds(col, row, zoom)
+                if prepared_geometry.intersects(geometry):
+                    tilelist.append((col, row))
+    
+        else:
+            print "ERROR: no valid geometry"
+            sys.exit(0)
+    
+        return tilelist
