318ba5c5e87823d61f3c3f5c571303a410044696
ungarj <joachim.ungar@gmail.com>
2015-08-18 17:08:16 +0200

    modify hillshading

--- tools/plugins/biggles.py
+++ tools/plugins/biggles.py
@@ -32,48 +32,80 @@ def process(metatile, parsed, metatilematrix):
     tiles = metatilematrix.tiles_from_tilematrix(zoom, col, row)
 
     metadata, rasterdata = read_raster_window(raster_file, metatilematrix, metatile,
-        pixelbuffer=10)
+        pixelbuffer=20)
 
     median = ndimage.median_filter(rasterdata, size=5)
     gauss = ndimage.gaussian_filter(median, 5)
-    hs = hillshade(gauss, 315, 65)
+
+    # hillshade 1
+    #hs1 = hillshade(gauss, 315, 45)
+    #gauss = ndimage.gaussian_filter(hs1, 0.5)
+    #hs1 = ndimage.median_filter(gauss, size=5)*1.5
+
+    # hillshade 2
+    hs2 = hillshade(gauss, 285, 45)
+    #gauss = ndimage.gaussian_filter(hs2, 0.5)
+    hs2 = ndimage.median_filter(hs2, size=5)
+
+    # hillshade 3
+    hs3 = hillshade(gauss, 345, 45)
+    #gauss = ndimage.gaussian_filter(hs3, 0.5)
+    hs3 = ndimage.median_filter(hs3, size=5)
+
+    hs = np.minimum(hs2, hs3)
+
     gauss = ndimage.gaussian_filter(hs, 0.5)
-    hs = ndimage.median_filter(gauss, size=5)
 
-    #laplace = ndimage.laplace(hs)
+    hs = gauss
 
-    #hs = laplace
+    if zoom > 30:
 
-    #sobel = ndimage.sobel(rasterdata)
+        ripple_threshold = 131
+        ripples = hillshade(rasterdata, 315, 90).astype(rasterio.uint8)
+        ripples[ripples == 0] = 255
+        ripples[ripples < ripple_threshold] = 1
+        ripples[ripples >= ripple_threshold] = 255
+    
+        gauss = ndimage.gaussian_filter(ripples, 1)
+    
+        ripples = gauss
+    
+        out = ndimage.gaussian_filter(ripples, 1)
+    
+        out = np.minimum(hs, ripples)
+    
+    else:
 
-    #hs = hillshade(correlate, 315, 65)
+        out = hs
 
-    if isinstance(hs, np.ndarray):
-        extension = metatilematrix.format.extension
+    
 
-        create_and_clean_dirs(metatile, parsed, extension)
-        out_tile = tile_path(metatile, parsed, extension)
-        try:
-            write_raster_window(out_tile, metatilematrix, metatile, metadata,
-                [hs], pixelbuffer=0)
-        except:
-            raise
-
-#    for tile in tiles:
-#        zoom, col, row = tile
+#    if isinstance(out, np.ndarray):
+#        extension = metatilematrix.format.extension
 #
-#        if isinstance(hs, np.ndarray):
-#            # Create directories.
-#            extension = metatilematrix.format.extension
-#
-#            create_and_clean_dirs(tile, parsed, extension)
-#
-#            out_tile = tile_path(tile, parsed, extension)
-#
-#            try:
-#                write_raster_window(out_tile, tilematrix, tile, metadata,
-#                    [hs], pixelbuffer=0)
-#            except:
-#                raise
-#        else:
-#            pass
+#        create_and_clean_dirs(metatile, parsed, extension)
+#        out_tile = tile_path(metatile, parsed, extension)
+#        try:
+#            write_raster_window(out_tile, metatilematrix, metatile, metadata,
+#                [out], pixelbuffer=0)
+#        except:
+#            raise
+
+    for tile in tiles:
+        zoom, col, row = tile
+
+        if isinstance(out, np.ndarray):
+            # Create directories.
+            extension = metatilematrix.format.extension
+
+            create_and_clean_dirs(tile, parsed, extension)
+
+            out_tile = tile_path(tile, parsed, extension)
+
+            try:
+                write_raster_window(out_tile, tilematrix, tile, metadata,
+                    [out], pixelbuffer=0)
+            except:
+                raise
+        else:
+            pass
