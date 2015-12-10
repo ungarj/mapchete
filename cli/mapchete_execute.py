9e294f75003cbcfe8c8e0d3acad1a9e4fd1f7e07
ungarj <joachim.ungar@gmail.com>
2015-12-10 11:00:52 +0100

    improving comments

--- cli/mapchete_execute.py
+++ cli/mapchete_execute.py
@@ -15,23 +15,21 @@ def main(args):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("process", type=str)
-    parser.add_argument("config", type=str)
+    parser.add_argument("config_yaml", type=str)
     parsed = parser.parse_args(args)
     process_file = parsed.process
-    config_yaml = parsed.config
+    config_yaml = parsed.config_yaml
 
     process_name = os.path.splitext(os.path.basename(process_file))[0]
 
-    # Load source process and initialize.
+    # Load source process from python file and initialize.
     new_process = imp.load_source(process_name + ".Process", process_file)
-    config = "herbert"
     user_defined_process = new_process.Process(config_yaml)
 
     print "processing", user_defined_process.identifier
 
-    for zoom in range(0, 21):
+    for zoom in range(0, 13):
         print zoom, user_defined_process.execute(zoom)
-        # user_defined_process.execute(zoom)
 
 
 if __name__ == "__main__":
--- test/merge_dems.py
+++ test/merge_dems.py
@@ -4,12 +4,15 @@ from src.mapchete import MapcheteProcess
 
 """
 User has to:
- - provide identifier
- - provide version
- - provide configuration object (?)
- - implement process
+ - execute(): implement process
+ - config_path: YAML configuration file
+optional:
+ - identifier
+ - title
+ - version
+ - abstract
 
-If the process gets executed, it runs at a certain zoom level.
+If the process gets executed, it only runs at a certain zoom level.
 """
 
 class Process(MapcheteProcess):
@@ -17,17 +20,15 @@ class Process(MapcheteProcess):
     Main process class which inherits from MapcheteProcess.
     """
     def __init__(self, config_path):
-        """
-        Process initialization from MapcheteProcess.
-        """
-
         MapcheteProcess.__init__(self, config_path)
-
         self.identifier = "merge_dems"
         self.title = "Merge multiple DEMs",
         self.version = "dirty pre-alpha",
         self.abstract = "Merges multiple DEMs into one.",
 
     def execute(self, zoom):
+        """
+        Here, the magic shall happen.
+        """
         params = self.config.at_zoom(zoom)
         return params
