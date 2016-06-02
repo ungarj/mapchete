a4b5ff1304219948a1446ef05f6b7b3968494bf3
joachim.ungar <joachim.ungar@eox.at>
2016-06-02 12:18:16 +0000

    implemented contour line generation using matplotlib

--- mapchete/commons.py
+++ mapchete/commons.py
@@ -6,7 +6,7 @@ import numpy as np
 import numpy.ma as ma
 from itertools import product
 from math import pi, sin, cos
-
+import matplotlib.pyplot as plt
 
 NODATA = -1
 
@@ -163,3 +163,63 @@ def clip_array_with_vector(
         )
 
     return masked_array
+
+
+def extract_contours(
+    array,
+    tile,
+    base=0,
+    interval=100,
+    pixelbuffer=0,
+    field='elev'
+    ):
+    """
+    Extracts contour lines from an array in a given interval and returns them
+    as GeoJSON-like objects using the source tile bounds as georeference
+    """
+    levels = _get_contour_values(array.min(), array.max(), interval=interval)
+    contours = plt.contour(array, levels)
+    index=0
+    out_contours = []
+    left, bottom, right, top = tile.bounds(pixelbuffer)
+    for level in range(len(contours.collections)):
+        elevation = levels[index]
+        index+=1
+        paths = contours.collections[level].get_paths()
+        for path in paths:
+            out_coords = [
+                (
+                    left+(i[1]*tile.pixel_x_size),
+                    top-(i[0]*tile.pixel_y_size),
+                )
+                for i in zip(path.vertices[:,1], path.vertices[:,0])
+            ]
+            line = LineString(out_coords)
+            out_contours.append({
+                    'properties': {
+                        field: elevation
+                    },
+                    'geometry': mapping(line)
+                }
+            )
+
+    return out_contours
+
+
+def _get_contour_values(min_val, max_val, base=0, interval=100):
+    """
+    Returns a list of values between min and max within an interval.
+    """
+    i = base
+    out = []
+
+    if min_val < base:
+        while i>=min_val:
+            i-=interval
+
+    while i<=max_val:
+        if i>=min_val:
+            out.append(i)
+        i+=interval
+
+    return out
