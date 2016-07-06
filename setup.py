89f6b8e27dd361df3116a6db1574659fc35562be
joachim.ungar <joachim.ungar@eox.at>
2016-07-06 10:21:15 +0000

    adding dependency version numbers & matplotlib

--- setup.py
+++ setup.py
@@ -26,15 +26,16 @@ setup(
     package_data={'static':['index.html']},
     install_requires=[
         'tilematrix',
-        'progressbar',
         'fiona',
         'pyyaml',
         'flask',
         'Pillow',
         'scipy',
         'psycopg2',
-        'blosc',
-        'bloscpack'
+        'blosc>=1.3.2',
+        'bloscpack',
+        'rasterio>=0.36.0',
+        'matplotlib'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
