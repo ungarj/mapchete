f6f4babbfb902d52b731030999b0ad62243b6913
joachim.ungar <joachim.ungar@eox.at>
2016-08-12 09:59:51 +0000

    split bounding boxes over antimeridian & stitch together numpy array; still bugs

--- mapchete/io_utils/raster_io.py
+++ mapchete/io_utils/raster_io.py
@@ -9,6 +9,9 @@ from rasterio.warp import transform_bounds, reproject
 import numpy as np
 import numpy.ma as ma
 from copy import deepcopy
+from affine import Affine
+from shapely.geometry import box
+from tilematrix import clip_geometry_to_srs_bounds
 
 from .io_funcs import RESAMPLING_METHODS
 from .numpy_io import write_numpy
@@ -21,42 +24,168 @@ def read_raster_window(
     resampling="nearest"
     ):
     """
-    Generates numpy arrays from input raster.
+    Generates numpy arrays (reprojected and resampled to tile properties) from
+    input raster. If tile boundaries cross the antimeridian, data on the other
+    side of the antimeridian will be read and concatenated to the numpy array
+    accordingly.
+    input_file: path to a raster file readable by rasterio.
+    tile: a Tile object
+    pixelbuffer: buffer around tile in pixels.
+    indexes: a list of band numbers; None will read all.
+    resampling: one of "nearest", "average", "bilinear" or "lanczos"
     """
     try:
         assert os.path.isfile(input_file)
-    except:
+    except AssertionError:
         raise IOError("input file not found %s" % input_file)
-    with rasterio.open(input_file, "r") as src:
 
-        if indexes:
-            if isinstance(indexes, list):
-                band_indexes = indexes
-            else:
-                band_indexes = [indexes]
+    if indexes:
+        if isinstance(indexes, list):
+            band_indexes = indexes
         else:
+            band_indexes = [indexes]
+    else:
+        with rasterio.open(input_file, "r") as src:
             band_indexes = src.indexes
 
-        if tile.crs == src.crs:
-            (src_left, src_bottom, src_right, src_top) = tile.bounds(
-                pixelbuffer=pixelbuffer
+    # Check if tile boundaries cross the antimeridian.
+    if pixelbuffer and not tile.bbox(pixelbuffer).within(
+        box(
+            tile.tile_pyramid.left,
+            tile.tile_pyramid.bottom,
+            tile.tile_pyramid.right,
+            tile.tile_pyramid.top
+            )
+        ):
+        tile_boxes = clip_geometry_to_srs_bounds(
+            tile.bbox(pixelbuffer),
+            tile.tile_pyramid
+            )
+        parts_metadata = {}
+        parts_metadata.update(
+            left=None,
+            both=None,
+            right=None
+        )
+        # Prepare top/bottom array extension if necessary.
+        touches_top = tile.top+pixelbuffer >= tile.tile_pyramid.top
+        touches_bottom = tile.bottom-pixelbuffer <= tile.tile_pyramid.bottom
+        if touches_top or touches_bottom:
+            with rasterio.open(input_file, "r") as src:
+                nodataval = src.nodata
+                # Quick fix because None nodata is not allowed.
+                if not nodataval:
+                    nodataval = 0
+                shape = (pixelbuffer, tile.width+2*pixelbuffer)
+                nodata_stripe = ma.masked_all(
+                    shape,
+                    src.dtypes[0]
+                    )
+
+        # Split bounding box into multiple parts & request each numpy array
+        # separately.
+        for polygon in tile_boxes:
+            part_metadata = {}
+            # Check on which side the antimeridian is touched by the polygon:
+            # left, both, right
+            left, bottom, right, top = polygon.bounds
+            touches_right = left == tile.tile_pyramid.left
+            touches_left = right == tile.tile_pyramid.right
+            touches_both = touches_left and touches_right
+            height = int(round((top-bottom)/tile.pixel_y_size))
+            width = int(round((right-left)/tile.pixel_x_size))
+            affine = Affine.translation(
+                left,
+                top
+                ) * Affine.scale(
+                tile.pixel_x_size,
+                -tile.pixel_y_size
                 )
+            part_metadata.update(
+                bounds=polygon.bounds,
+                shape=(height, width),
+                affine=affine
+            )
+            if touches_both:
+                parts_metadata.update(both=part_metadata)
+            elif touches_left:
+                parts_metadata.update(left=part_metadata)
+            elif touches_right:
+                parts_metadata.update(right=part_metadata)
+
+        # Finally, stitch numpy arrays together into one.
+        for band_idx in band_indexes:
+            stitched = ma.concatenate(
+                [
+                _get_warped_array(
+                    input_file=input_file,
+                    band_idx=band_idx,
+                    dst_bounds=parts_metadata[part]["bounds"],
+                    dst_shape=parts_metadata[part]["shape"],
+                    dst_affine=parts_metadata[part]["affine"],
+                    dst_crs=tile.crs,
+                    resampling=resampling
+                    )
+                for part in ["left", "both", "right"]
+                if parts_metadata[part]
+                ],
+                axis=1
+            )
+            # Extend array to assert it has the expected tile shape.
+            if touches_top:
+                stitched = ma.concatenate([nodata_stripe, stitched], axis=0)
+            if touches_bottom:
+                stitched = ma.concatenate([stitched, nodata_stripe], axis=0)
+            assert stitched.shape == tile.shape(pixelbuffer)
+
+            yield stitched
+
+    else:
+        # Otherwise, simply read window once.
+        for band_idx in band_indexes:
+            yield _get_warped_array(
+                input_file=input_file,
+                band_idx=band_idx,
+                dst_bounds=tile.bounds(pixelbuffer),
+                dst_shape=tile.shape(pixelbuffer),
+                dst_affine=tile.affine(pixelbuffer),
+                dst_crs=tile.crs,
+                resampling=resampling
+                )
+
+def _get_warped_array(
+    input_file=None,
+    band_idx=None,
+    dst_bounds=None,
+    dst_shape=None,
+    dst_affine=None,
+    dst_crs=None,
+    resampling="nearest"):
+    """
+    Extracts a numpy array from a raster file.
+    """
+    assert isinstance(input_file, str)
+    assert isinstance(band_idx, int)
+    assert isinstance(dst_bounds, tuple)
+    assert isinstance(dst_shape, tuple)
+    assert isinstance(dst_affine, Affine)
+    assert isinstance(dst_crs, dict)
+
+    with rasterio.open(input_file, "r") as src:
+        if dst_crs == src.crs:
+            src_left, src_bottom, src_right, src_top = dst_bounds
         else:
             # Reproject tile bounds to source file SRS.
             src_left, src_bottom, src_right, src_top = transform_bounds(
-                tile.crs,
+                dst_crs,
                 src.crs,
-                *tile.bounds(pixelbuffer=pixelbuffer),
+                *dst_bounds,
                 densify_pts=21
                 )
-            # TODO: find better fix to avoid invalid bounds values
-            if float('Inf') in (src_left, src_bottom, src_right, src_top):
-                src_left, src_bottom, src_right, src_top = transform_bounds(
-                    tile.crs,
-                    src.crs,
-                    *tile.bounds(pixelbuffer=0),
-                    densify_pts=21
-                    )
+        if float('Inf') in (src_left, src_bottom, src_right, src_top):
+            raise ValueError(
+                "Tile boundaries could not be translated into source file SRS."
+                )
 
         nodataval = src.nodata
         # Quick fix because None nodata is not allowed.
@@ -72,35 +201,37 @@ def read_raster_window(
             minrow
             )
         window_affine = src.affine * window_vector_affine
-        # Finally read data per band and store it in tuple.
-        bands = (
-            src.read(index, window=window, masked=True, boundless=True)
-            for index in band_indexes
+
+        # Finally read data.
+        src_band = src.read(
+            band_idx,
+            window=window,
+            masked=True,
+            boundless=True
             )
-        for index in band_indexes:
-            dst_band = ma.zeros(
-                (tile.shape(pixelbuffer=pixelbuffer)),
-                src.dtypes[index-1]
+        dst_band = ma.zeros(
+                dst_shape,
+                src.dtypes[band_idx-1]
             )
-            dst_band[:] = nodataval
-            reproject(
-                next(bands),
-                dst_band,
-                src_transform=window_affine,
-                src_crs=src.crs,
-                src_nodata=nodataval,
-                dst_transform=tile.affine(pixelbuffer=pixelbuffer),
-                dst_crs=tile.crs,
-                dst_nodata=nodataval,
-                resampling=RESAMPLING_METHODS[resampling]
-            )
-            dst_band = ma.masked_equal(dst_band, nodataval)
-            dst_band = ma.masked_array(
-                dst_band,
-                mask=ma.fix_invalid(dst_band, fill_value=0).mask
-            )
-            dst_band.harden_mask()
-            yield dst_band
+        dst_band[:] = nodataval
+        reproject(
+            src_band,
+            dst_band,
+            src_transform=window_affine,
+            src_crs=src.crs,
+            src_nodata=nodataval,
+            dst_transform=dst_affine,
+            dst_crs=dst_crs,
+            dst_nodata=nodataval,
+            resampling=RESAMPLING_METHODS[resampling]
+        )
+        dst_band = ma.masked_equal(dst_band, nodataval)
+        dst_band = ma.masked_array(
+            dst_band,
+            mask=ma.fix_invalid(dst_band, fill_value=0).mask
+        )
+        dst_band.harden_mask()
+        return dst_band
 
 def write_raster(
     process,
