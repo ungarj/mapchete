f0e540a49fe0cf2d8d539f9cb11e67cb7d0b0e73
Joachim Ungar <joachim.ungar@eox.at>
2017-11-23 14:15:38 +0100

    allow rasterio versions > 1.0a10

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         'pyyaml',
         'flask',
         'Pillow',
-        'rasterio==1.0a10',
+        'rasterio>=1.0a10',
         'matplotlib',
         'cached_property',
         'pyproj',
