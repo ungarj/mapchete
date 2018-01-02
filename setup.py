1891eedb4a0245e8052b49b0db7632c1a4e003bd
Joachim Ungar <joachim.ungar@eox.at>
2018-01-02 15:21:45 +0100

    removed python 3.3 from tests, updated package metadata

--- setup.py
+++ setup.py
@@ -71,6 +71,8 @@ setup(
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     setup_requires=['pytest-runner'],
