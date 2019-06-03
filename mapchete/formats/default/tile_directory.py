d26c2ca79213e385a30e733ac7fddbf4ee808ead
Joachim Ungar <joachim.ungar@eox.at>
2019-06-03 15:42:40 +0200

    move tiledirectory interpolation parameters from open() to read() function

--- mapchete/formats/default/tile_directory.py
+++ mapchete/formats/default/tile_directory.py
@@ -131,12 +131,6 @@ class InputData(base.InputData):
     def open(
         self,
         tile,
-        tile_directory_zoom=None,
-        matching_method="gdal",
-        matching_max_zoom=None,
-        matching_precision=8,
-        fallback_to_higher_zoom=False,
-        resampling="nearest",
         **kwargs
     ):
         """
@@ -145,82 +139,19 @@ class InputData(base.InputData):
         Parameters
         ----------
         tile : ``Tile``
-        tile_directory_zoom : None
-            If set, data will be read from exactly this zoom level
-        matching_method : str ('gdal' or 'min') (default: 'gdal')
-            gdal: Uses GDAL's standard method. Here, the target resolution is calculated
-                by averaging the extent's pixel sizes over both x and y axes. This
-                approach returns a zoom level which may not have the best quality but will
-                speed up reading significantly.
-            min: Returns the zoom level which matches the minimum resolution of the
-                extents four corner pixels. This approach returns the zoom level with the
-                best possible quality but with low performance. If the tile extent is
-                outside of the destination pyramid, a TopologicalError will be raised.
-        matching_max_zoom : int (default: None)
-            If set, it will prevent reading from zoom levels above the maximum.
-        matching_precision : int
-            Round resolutions to n digits before comparing.
-        fallback_to_higher_zoom : bool (default: False)
-            In case no data is found at zoom level, try to read data from higher zoom
-            levels. Enabling this setting can lead to many IO requests in areas with no
-            data.
-        resampling : string
-            raster file: one of "nearest", "average", "bilinear" or "lanczos"
 
         Returns
         -------
         input tile : ``InputTile``
             tile view of input data
         """
-        # determine tile bounds in TileDirectory CRS
-        td_bounds = reproject_geometry(
-            tile.bbox,
-            src_crs=tile.tp.crs,
-            dst_crs=self.td_pyramid.crs
-        ).bounds
-
-        # find target zoom level
-        if tile_directory_zoom is not None:
-            zoom = tile_directory_zoom
-        else:
-            zoom = tile_to_zoom_level(
-                tile,
-                dst_pyramid=self.td_pyramid,
-                matching_method=matching_method,
-                precision=matching_precision
-            )
-            if matching_max_zoom is not None:
-                zoom = min([zoom, matching_max_zoom])
-
-        if fallback_to_higher_zoom:
-            tiles_paths = []
-            # check if tiles exist otherwise try higher zoom level
-            while len(tiles_paths) == 0 and zoom >= 0:
-                tiles_paths = _get_tiles_paths(
-                    basepath=self.path,
-                    ext=self._ext,
-                    pyramid=self.td_pyramid,
-                    bounds=td_bounds,
-                    zoom=zoom
-                )
-                logger.debug("%s existing tiles found at zoom %s", len(tiles_paths), zoom)
-                zoom -= 1
-        else:
-            tiles_paths = _get_tiles_paths(
-                basepath=self.path,
-                ext=self._ext,
-                pyramid=self.td_pyramid,
-                bounds=td_bounds,
-                zoom=zoom
-            )
-            logger.debug("%s existing tiles found at zoom %s", len(tiles_paths), zoom)
         return InputTile(
             tile,
-            tiles_paths=tiles_paths,
             file_type=self._file_type,
+            basepath=self.path,
+            file_extension=self._ext,
             profile=self._profile,
-            td_crs=self.td_pyramid.crs,
-            resampling=resampling,
+            td_pyramid=self.td_pyramid,
             read_as_tiledir_func=self._read_as_tiledir_func,
             **kwargs
         )
@@ -277,21 +208,36 @@ class InputTile(base.InputTile):
     tile : tile : ``Tile``
     """
 
-    def __init__(self, tile, **kwargs):
+    def __init__(
+        self,
+        tile,
+        file_type=None,
+        basepath=None,
+        file_extension=None,
+        profile=None,
+        td_crs=None,
+        td_pyramid=None,
+        read_as_tiledir_func=None
+    ):
         """Initialize."""
         self.tile = tile
-        self._tiles_paths = kwargs["tiles_paths"]
-        self._file_type = kwargs["file_type"]
-        self._profile = kwargs["profile"]
-        self._td_crs = kwargs["td_crs"]
-        self._resampling = kwargs["resampling"]
-        self._read_as_tiledir = kwargs["read_as_tiledir_func"]
+        self._file_type = file_type
+        self._basepath = basepath
+        self._ext = file_extension
+        self._profile = profile
+        self._td_pyramid = td_pyramid
+        self._read_as_tiledir = read_as_tiledir_func
 
     def read(
         self,
+        tile_directory_zoom=None,
+        matching_method="gdal",
+        matching_max_zoom=None,
+        matching_precision=8,
+        fallback_to_higher_zoom=False,
+        resampling="nearest",
         validity_check=False,
         indexes=None,
-        resampling=None,
         dst_nodata=None,
         gdal_opts=None,
         **kwargs
@@ -301,10 +247,30 @@ class InputTile(base.InputTile):
 
         Parameters
         ----------
+        tile_directory_zoom : None
+            If set, data will be read from exactly this zoom level
+        matching_method : str ('gdal' or 'min') (default: 'gdal')
+            gdal: Uses GDAL's standard method. Here, the target resolution is calculated
+                by averaging the extent's pixel sizes over both x and y axes. This
+                approach returns a zoom level which may not have the best quality but will
+                speed up reading significantly.
+            min: Returns the zoom level which matches the minimum resolution of the
+                extents four corner pixels. This approach returns the zoom level with the
+                best possible quality but with low performance. If the tile extent is
+                outside of the destination pyramid, a TopologicalError will be raised.
+        matching_max_zoom : int (default: None)
+            If set, it will prevent reading from zoom levels above the maximum.
+        matching_precision : int
+            Round resolutions to n digits before comparing.
+        fallback_to_higher_zoom : bool (default: False)
+            In case no data is found at zoom level, try to read data from higher zoom
+            levels. Enabling this setting can lead to many IO requests in areas with no
+            data.
+        resampling : string
+            raster file: one of "nearest", "average", "bilinear" or "lanczos"
         validity_check : bool
             vector file: also run checks if reprojected geometry is valid,
             otherwise throw RuntimeError (default: True)
-
         indexes : list or int
             raster file: a list of band numbers; None will read all.
         dst_nodata : int or float, optional
@@ -320,23 +286,119 @@ class InputTile(base.InputTile):
         return self._read_as_tiledir(
             data_type=self._file_type,
             out_tile=self.tile,
-            td_crs=self._td_crs,
-            tiles_paths=self._tiles_paths,
+            td_crs=self._td_pyramid.crs,
+            tiles_paths=self._get_tiles_paths(
+                tile_directory_zoom=tile_directory_zoom,
+                fallback_to_higher_zoom=fallback_to_higher_zoom,
+                matching_method=matching_method,
+                matching_precision=matching_precision,
+                matching_max_zoom=matching_max_zoom,
+            ),
             profile=self._profile,
             validity_check=validity_check,
             indexes=indexes,
-            resampling=resampling if resampling else self._resampling,
+            resampling=resampling,
             dst_nodata=dst_nodata,
             gdal_opts=gdal_opts,
             **{k: v for k, v in kwargs.items() if k != "data_type"}
         )
 
-    def is_empty(self):
+    def is_empty(
+        self,
+        tile_directory_zoom=None,
+        fallback_to_higher_zoom=False,
+        matching_method="gdal",
+        matching_precision=8,
+        matching_max_zoom=None,
+    ):
         """
         Check if there is data within this tile.
+
+        Parameters
+        ----------
+        tile_directory_zoom : None
+            If set, data will be read from exactly this zoom level
+        matching_method : str ('gdal' or 'min') (default: 'gdal')
+            gdal: Uses GDAL's standard method. Here, the target resolution is calculated
+                by averaging the extent's pixel sizes over both x and y axes. This
+                approach returns a zoom level which may not have the best quality but will
+                speed up reading significantly.
+            min: Returns the zoom level which matches the minimum resolution of the
+                extents four corner pixels. This approach returns the zoom level with the
+                best possible quality but with low performance. If the tile extent is
+                outside of the destination pyramid, a TopologicalError will be raised.
+        matching_max_zoom : int (default: None)
+            If set, it will prevent reading from zoom levels above the maximum.
+        matching_precision : int
+            Round resolutions to n digits before comparing.
+        fallback_to_higher_zoom : bool (default: False)
+            In case no data is found at zoom level, try to read data from higher zoom
+            levels. Enabling this setting can lead to many IO requests in areas with no
+            data.
 
         Returns
         -------
         is empty : bool
         """
-        return len(self._tiles_paths) == 0
+        return len(
+            self._get_tiles_paths(
+                tile_directory_zoom=tile_directory_zoom,
+                fallback_to_higher_zoom=fallback_to_higher_zoom,
+                matching_method=matching_method,
+                matching_precision=matching_precision,
+                matching_max_zoom=matching_max_zoom,
+            )
+        ) == 0
+
+    def _get_tiles_paths(
+        self,
+        tile_directory_zoom=None,
+        fallback_to_higher_zoom=False,
+        matching_method="gdal",
+        matching_precision=8,
+        matching_max_zoom=None,
+    ):
+        # determine tile bounds in TileDirectory CRS
+        td_bounds = reproject_geometry(
+            self.tile.bbox,
+            src_crs=self.tile.tp.crs,
+            dst_crs=self._td_pyramid.crs
+        ).bounds
+
+        # find target zoom level
+        if tile_directory_zoom is None:
+            zoom = tile_to_zoom_level(
+                self.tile,
+                dst_pyramid=self._td_pyramid,
+                matching_method=matching_method,
+                precision=matching_precision
+            )
+            if matching_max_zoom is not None:
+                zoom = min([zoom, matching_max_zoom])
+        else:
+            zoom = tile_directory_zoom
+
+        if fallback_to_higher_zoom:
+            tiles_paths = []
+            # check if tiles exist otherwise try higher zoom level
+            while len(tiles_paths) == 0 and zoom >= 0:
+                tiles_paths = _get_tiles_paths(
+                    basepath=self._basepath,
+                    ext=self._ext,
+                    pyramid=self._td_pyramid,
+                    bounds=td_bounds,
+                    zoom=zoom
+                )
+                logger.debug("%s existing tiles found at zoom %s", len(tiles_paths), zoom)
+                zoom -= 1
+        else:
+            tiles_paths = _get_tiles_paths(
+                basepath=self._basepath,
+                ext=self._ext,
+                pyramid=self._td_pyramid,
+                bounds=td_bounds,
+                zoom=zoom
+            )
+            logger.debug("%s existing tiles found at zoom %s", len(tiles_paths), zoom)
+
+        return tiles_paths
