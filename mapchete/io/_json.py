9f382e75c5b7ab8d1f4f17f9dbc08650b98a1054
Joachim Ungar <joachim.ungar@eox.at>
2020-11-19 18:19:04 +0100

    fix broken log message

--- mapchete/io/_json.py
+++ mapchete/io/_json.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 def write_json(path, params, fs=None, **kwargs):
     """Write local or remote."""
-    logger.debug(f"write {params} to {path}", params, path)
+    logger.debug(f"write {params} to {path}")
     with fsspec.open(path, "w") as dst:
         json.dump(params, dst, sort_keys=True, indent=4)
 
