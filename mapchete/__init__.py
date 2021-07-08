6e84f7678dacf445b8ddd8e11e4f26adce5443cb
Joachim Ungar <joachim.ungar@eox.at>
2021-07-08 13:22:21 +0200

    add ResultGenerator class

--- mapchete/__init__.py
+++ mapchete/__init__.py
@@ -1,12 +1,12 @@
 import logging
 
 from mapchete._core import open, Mapchete
-from mapchete._processing import MapcheteProcess
+from mapchete._processing import MapcheteProcess, ProcessInfo
 from mapchete.tile import count_tiles
 from mapchete._timer import Timer
 
 
-__all__ = ["open", "count_tiles", "Mapchete", "MapcheteProcess", "Timer"]
+__all__ = ["open", "count_tiles", "Mapchete", "MapcheteProcess", "ProcessInfo", "Timer"]
 __version__ = "0.40"
 
 
