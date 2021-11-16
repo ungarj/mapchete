96f3a1358c95b39a619cc6ab7f3116328a74463b
Joachim Ungar <joachim.ungar@eox.at>
2021-11-16 11:28:47 +0100

    bump version; update changelog

--- mapchete/__init__.py
+++ mapchete/__init__.py
@@ -8,7 +8,7 @@ from mapchete._timer import Timer
 
 
 __all__ = ["open", "count_tiles", "Mapchete", "MapcheteProcess", "ProcessInfo", "Timer"]
-__version__ = "2021.11.0"
+__version__ = "2021.11.1"
 
 
 logger = logging.getLogger(__name__)
