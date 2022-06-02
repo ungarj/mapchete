ae1f44832aed7dfb7555e792a9e74db1a03f1111
Joachim Ungar <joachim.ungar@eox.at>
2022-06-02 20:07:24 +0200

    remove test line

--- mapchete/processes/hillshade.py
+++ mapchete/processes/hillshade.py
@@ -65,7 +65,6 @@ def execute(
     ------
     np.ndarray
     """
-    raise NotImplementedError("halt, stop")
     # read clip geometry
     if "clip" in mp.params["input"]:
         clip_geom = mp.open("clip").read()
