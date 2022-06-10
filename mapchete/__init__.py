d1ae01cae5ac1219670306d536491084eb709413
Joachim Ungar <joachim.ungar@eox.at>
2022-06-10 16:08:30 +0200

    bump version; update changelog

--- mapchete/__init__.py
+++ mapchete/__init__.py
@@ -20,7 +20,7 @@ __all__ = [
     "SkippedFuture",
     "Job",
 ]
-__version__ = "2022.4.1"
+__version__ = "2022.6.0"
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
