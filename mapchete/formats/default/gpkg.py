5aea7933fdae4ba9ed56a653bccb15020b9511c4
Joachim Ungar <joachim.ungar@eox.at>
2022-09-08 15:37:07 +0200

    add vector single output file classes

---
+++ mapchete/formats/default/gpkg.py
@@ -0,0 +1,104 @@
+"""
+Handles writing process output into GPKG files.
+
+output configuration parameters
+-------------------------------
+
+mandatory
+~~~~~~~~~
+
+path: string
+    output directory
+schema: key-value pairs
+    the schema is passed on to fiona
+    - properties: key-value pairs (fields and field types, like "id: int" etc.)
+    - geometry: output geometry type (Geometry, Point, MultiPoint, Line, MultiLine,
+    Polygon, MultiPolygon)
+"""
+
+from mapchete.formats.default import _fiona_base
+
+METADATA = {"driver_name": "GPKG", "data_type": "vector", "mode": "rw"}
+
+
+class OutputDataReader(_fiona_base.OutputDataReader):
+    """
+    Output reader class for GPKG.
+
+    Parameters
+    ----------
+    output_params : dictionary
+        output parameters from Mapchete file
+
+    Attributes
+    ----------
+    path : string
+        path to output directory
+    file_extension : string
+        file extension for output files (.gpkg)
+    output_params : dictionary
+        output parameters from Mapchete file
+    pixelbuffer : integer
+        buffer around output tiles
+    pyramid : ``tilematrix.TilePyramid``
+        output ``TilePyramid``
+    crs : ``rasterio.crs.CRS``
+        object describing the process coordinate reference system
+    srid : string
+        spatial reference ID of CRS (e.g. "{'init': 'epsg:4326'}")
+    """
+
+    METADATA = METADATA
+
+    def __init__(self, output_params, **kwargs):
+        """Initialize."""
+        super().__init__(output_params)
+        self.path = output_params["path"]
+        self.file_extension = ".geojson"
+        self.output_params = output_params
+        self._bucket = (
+            self.path.split("/")[2] if self.path.startswith("s3://") else None
+        )
+
+
+class TileDirectoryOutputDataWriter(
+    _fiona_base.TileDirectoryOutputDataWriter, OutputDataReader
+):
+
+    METADATA = METADATA
+
+
+class SingleFileOutputDataWriter(
+    _fiona_base.SingleFileOutputDataWriter, OutputDataReader
+):
+
+    METADATA = METADATA
+
+
+class OutputDataWriter:
+    def __new__(self, output_params, **kwargs):
+        """Initialize."""
+        self.path = output_params["path"]
+        self.file_extension = ".gpkg"
+        if self.path.endswith(self.file_extension):
+            return SingleFileOutputDataWriter(output_params, **kwargs)
+        else:
+            return TileDirectoryOutputDataWriter(output_params, **kwargs)
+
+
+class InputTile(_fiona_base.InputTile):
+    """
+    Target Tile representation of input data.
+
+    Parameters
+    ----------
+    tile : ``Tile``
+    process : ``MapcheteProcess``
+
+    Attributes
+    ----------
+    tile : ``Tile``
+    process : ``MapcheteProcess``
+    """
+
+    pass
