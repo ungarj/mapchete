680de0dcb6347428e6126f384e33e51c3236b461
Joachim Ungar <joachim.ungar@gmail.com>
2022-12-28 11:23:12 +0100

    use deprecated select interface for Python 3.9 and lower

--- mapchete/_registered.py
+++ mapchete/_registered.py
@@ -5,6 +5,11 @@ except ImportError:  # pragma: no cover
     import importlib_metadata as metadata
 
 entry_points = metadata.entry_points()
-commands = entry_points.select(group="mapchete.cli.commands")
-drivers = entry_points.select(group="mapchete.formats.drivers")
-processes = entry_points.select(group="mapchete.processes")
+if hasattr(entry_points, "select"):  # for Python 3.10 and higher
+    commands = entry_points.select(group="mapchete.cli.commands")
+    drivers = entry_points.select(group="mapchete.formats.drivers")
+    processes = entry_points.select(group="mapchete.processes")
+else:  # for Python 3.9 and lower
+    commands = entry_points.get("mapchete.cli.commands", {})
+    drivers = entry_points.get("mapchete.formats.drivers", {})
+    processes = entry_points.get("mapchete.processes", {})
