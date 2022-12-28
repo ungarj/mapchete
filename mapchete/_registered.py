114379de1c08cda4f38a3281c0cd7404b22bc48b
Joachim Ungar <joachim.ungar@gmail.com>
2022-12-28 11:15:26 +0100

    use new importlib.metadata interface to select entry points

--- mapchete/_registered.py
+++ mapchete/_registered.py
@@ -4,6 +4,7 @@ except ImportError:  # pragma: no cover
     # <PY38 use backport
     import importlib_metadata as metadata
 
-commands = metadata.entry_points().get("mapchete.cli.commands", ())
-drivers = metadata.entry_points().get("mapchete.formats.drivers", ())
-processes = metadata.entry_points().get("mapchete.processes", ())
+entry_points = metadata.entry_points()
+commands = entry_points.select(group="mapchete.cli.commands")
+drivers = entry_points.select(group="mapchete.formats.drivers")
+processes = entry_points.select(group="mapchete.processes")
