a55d73bab942b92cfb2e38c7b2203c9a03512a4e
Joachim Ungar <joachim.ungar@gmail.com>
2023-05-04 12:50:43 +0200

    add deprecated mapchete.io._path module to be backwards compatible

---
+++ mapchete/io/_path.py
@@ -0,0 +1,24 @@
+"""This module is deprecated and only here for backwards compatibility"""
+
+from mapchete.path import (
+    fs_from_path,
+    path_is_remote,
+    path_exists,
+    tiles_exist,
+    absolute_path,
+    relative_path,
+    makedirs,
+    MPath,
+)
+
+
+__all__ = [
+    "fs_from_path",
+    "path_is_remote",
+    "path_exists",
+    "tiles_exist",
+    "absolute_path",
+    "relative_path",
+    "makedirs",
+    "MPath",
+]
