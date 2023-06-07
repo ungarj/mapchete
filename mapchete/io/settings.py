8526a0e271d185f3ed366610f6f4eb2ce04386fa
Joachim Ungar <joachim.ungar@gmail.com>
2023-06-07 14:51:30 +0200

    deactivate setting which lets tests stall

--- mapchete/io/settings.py
+++ mapchete/io/settings.py
@@ -14,11 +14,11 @@ GDAL_HTTP_DEFAULTS = dict(
     # this will be set later on depending on the opened file
     CPL_VSIL_CURL_ALLOWED_EXTENSIONS="",
     # 200MB
-    CPL_VSIL_CURL_CACHE_SIZE=200000000,
+    CPL_VSIL_CURL_CACHE_SIZE=200_000_000,
     # alternative: ARRAY
     GDAL_BAND_BLOCK_CACHE="HASHSET",
-    # 200MB
-    GDAL_CACHEMAX=200,
+    # # 200MB
+    # GDAL_CACHEMAX=200, --> activating this seems to let the tests stall at some point
     # don't make LIST request
     GDAL_DISABLE_READDIR_ON_OPEN="EMPTY_DIR",
     GDAL_HTTP_TIMEOUT=30,
@@ -30,7 +30,7 @@ GDAL_HTTP_DEFAULTS = dict(
     # let GDAL cache internally
     VSI_CACHE=True,
     # 5MB cache per file
-    VSI_CACHE_SIZE=5000000,
+    VSI_CACHE_SIZE=5_000_000,
 )
 GDAL_HTTP_OPTS = _merge_gdal_defaults_with_env()
 MAPCHETE_IO_RETRY_SETTINGS = {
