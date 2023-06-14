e03a069fed523d28da5540f92f3a9c491ba5d50d
Joachim Ungar <joachim.ungar@gmail.com>
2023-06-14 11:39:43 +0200

    Release 2023.6.5

--- mapchete/__init__.py
+++ mapchete/__init__.py
@@ -19,7 +19,7 @@ __all__ = [
     "SkippedFuture",
     "Job",
 ]
-__version__ = "2023.6.4"
+__version__ = "2023.6.5"
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
