d56a0efa4b2665224c164f6f740f62ad64f6e077
Joachim Ungar <joachim.ungar@gmail.com>
2023-09-19 16:17:53 +0200

    convert default GDAL HTTP options into a basesettings model

--- mapchete/io/settings.py
+++ mapchete/io/settings.py
@@ -1,40 +1,34 @@
-import logging
-import os
+"""
+Combine default values with environment variable values.
+"""
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-logger = logging.getLogger(__name__)
-
-
-def _merge_gdal_defaults_with_env():
-    return {k: os.environ.get(k, v) for k, v in GDAL_HTTP_DEFAULTS.items()}
 
 
 # defaults sets according to the recommendations given at
 # https://developmentseed.org/titiler/advanced/performance_tuning/
-GDAL_HTTP_DEFAULTS = dict(
+class GDALHTTPOptions(BaseSettings):
     # this will be set later on depending on the opened file
-    CPL_VSIL_CURL_ALLOWED_EXTENSIONS="",
+    CPL_VSIL_CURL_ALLOWED_EXTENSIONS: str = ""
     # 200MB
-    CPL_VSIL_CURL_CACHE_SIZE=200_000_000,
+    CPL_VSIL_CURL_CACHE_SIZE: int = 200_000_000
     # alternative: ARRAY
-    GDAL_BAND_BLOCK_CACHE="HASHSET",
+    GDAL_BAND_BLOCK_CACHE: str = "HASHSET"
     # # 200MB
     # GDAL_CACHEMAX=200, --> activating this seems to let the tests stall at some point
     # don't make LIST request
-    GDAL_DISABLE_READDIR_ON_OPEN="EMPTY_DIR",
-    GDAL_HTTP_TIMEOUT=30,
-    GDAL_HTTP_MAX_RETRY=3,
-    GDAL_HTTP_MERGE_CONSECUTIVE_RANGES=True,
-    GDAL_HTTP_MULTIPLEX=True,
-    GDAL_HTTP_RETRY_DELAY=5,
-    GDAL_HTTP_VERSION=2,
+    GDAL_DISABLE_READDIR_ON_OPEN: str = "EMPTY_DIR"
+    GDAL_HTTP_TIMEOUT: int = 30
+    GDAL_HTTP_MAX_RETRY: int = 3
+    GDAL_HTTP_MERGE_CONSECUTIVE_RANGES: bool = True
+    GDAL_HTTP_MULTIPLEX: bool = True
+    GDAL_HTTP_RETRY_DELAY: int = 5
+    GDAL_HTTP_VERSION: int = 2
     # let GDAL cache internally
-    VSI_CACHE=True,
+    VSI_CACHE: bool = True
     # 5MB cache per file
-    VSI_CACHE_SIZE=5_000_000,
-)
-GDAL_HTTP_OPTS = _merge_gdal_defaults_with_env()
+    VSI_CACHE_SIZE: int = 5_000_000
+    model_config = SettingsConfigDict()
 
 
 class IORetrySettings(BaseSettings):
