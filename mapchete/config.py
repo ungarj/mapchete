0e37dc30436fe3c85418c4fd05714327c214e8e0
Joachim Ungar <joachim.ungar@gmail.com>
2023-09-19 16:38:16 +0200

    change type ordering

--- mapchete/config.py
+++ mapchete/config.py
@@ -57,7 +57,7 @@ class OutputConfigBase(BaseModel):
     metatiling: Union[int, None] = 1
     pixelbuffer: Union[NonNegativeInt, None] = 0
 
-    @field_validator("metatiling", mode='before')
+    @field_validator("metatiling", mode="before")
     def _metatiling(cls, value: int) -> int:  # pragma: no cover
         _metatiling_opts = [2**x for x in range(10)]
         if value not in _metatiling_opts:
@@ -70,7 +70,7 @@ class PyramidConfig(BaseModel):
     metatiling: Union[int, None] = 1
     pixelbuffer: Union[NonNegativeInt, None] = 0
 
-    @field_validator("metatiling", mode='before')
+    @field_validator("metatiling", mode="before")
     def _metatiling(cls, value: int) -> int:  # pragma: no cover
         _metatiling_opts = [2**x for x in range(10)]
         if value not in _metatiling_opts:
@@ -88,7 +88,7 @@ class ProcessConfig(BaseModel, arbitrary_types_allowed=True):
     config_dir: Union[str, MPath, None] = None
     area: Union[str, MPath, BaseGeometry, None] = None
     area_crs: Union[dict, str, None] = None
-    bounds: Union[Tuple[float, float, float, float], Bounds, None] = None
+    bounds: Union[Bounds, Tuple[float, float, float, float], None] = None
     bounds_crs: Union[dict, str, None] = None
     process_parameters: Union[dict, None] = None
 
