f8c4902f48c7ac8680d56af1da5535b7150d8bf8
Joachim Ungar <joachim.ungar@gmail.com>
2024-06-04 12:03:07 +0200

    remove legacy code from code coverage

--- mapchete/registered.py
+++ mapchete/registered.py
@@ -1,7 +1,7 @@
 try:
     from importlib import metadata
 except ImportError:  # pragma: no cover
-    # <PY38 use backport
+    # backport for Python 3.8 and lower
     import importlib_metadata as metadata
 
 entry_points = metadata.entry_points()
@@ -9,7 +9,8 @@ if hasattr(entry_points, "select"):  # for Python 3.10 and higher
     commands = entry_points.select(group="mapchete.cli.commands")
     drivers = entry_points.select(group="mapchete.formats.drivers")
     processes = entry_points.select(group="mapchete.processes")
-else:  # for Python 3.9 and lower
-    commands = entry_points.get("mapchete.cli.commands", {})
-    drivers = entry_points.get("mapchete.formats.drivers", {})
-    processes = entry_points.get("mapchete.processes", {})
+# for Python 3.9 and lower
+else:  # pragma: no cover
+    commands = entry_points.get("mapchete.cli.commands", {})  # type: ignore
+    drivers = entry_points.get("mapchete.formats.drivers", {})  # type: ignore
+    processes = entry_points.get("mapchete.processes", {})  # type: ignore
