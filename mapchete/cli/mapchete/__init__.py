814bce40865ff8f15d4c533e2005e86cf562d8ec
Joachim Ungar <joachim.ungar@gmail.com>
2025-05-19 08:48:36 +0200

    properly move everything into mapchete module

---
+++ mapchete/cli/mapchete/__init__.py
@@ -0,0 +1,14 @@
+"""Mapchete command line tool with subcommands."""
+
+import click
+from click_plugins import with_plugins
+
+from mapchete import __version__
+from mapchete.registered import commands
+
+
+@with_plugins(commands)
+@click.version_option(version=__version__, message="%(version)s")
+@click.group()
+def main():
+    pass
